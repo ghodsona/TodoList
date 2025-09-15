68efc08134e607e527864508ad2bb79c04dfb265
Ghodsona <zeynab.q85@gmail.com>
2025-09-15 12:07:15 +0330

    board first model

---
+++ shared/src/main/java/shared/Model/Board.java
@@ -0,0 +1,27 @@
+package shared.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Board extends Model {
+    private String name;
+    private UUID ownerId;
+    private List<UUID> memberIds = new ArrayList<>();
+    public Board() {}
+
+    public Board(String name, UUID ownerId) {
+        this.name = name;
+        this.ownerId = ownerId;
+        this.memberIds.add(ownerId);
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public UUID getOwnerId() { return ownerId; }
+    public void setOwnerId(UUID ownerId) { this.ownerId = ownerId; }
+
+    public List<UUID> getMemberIds() { return memberIds; }
+    public void setMemberIds(List<UUID> memberIds) { this.memberIds = memberIds; }
+}
